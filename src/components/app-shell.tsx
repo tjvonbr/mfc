567d074fc4a29dba6263ac44639152db0c87f6e6
Trevor Von Bruenchenhein <tjvonbr@gmail.com>
2020-12-06 12:29:58 -0600

    initial commit

---
+++ src/components/app-shell.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AppShell: React.FC = ({ children }) => {
+  return (
+    <div className="app-shell">
+      {children}
+    </div>
+  )
+}
+
+export default AppShell
